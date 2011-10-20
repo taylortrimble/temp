b6fa1a0df94432fe68d9630f0e7350f5e13b3036
Taylor Trimble <tylrtrmbl@gmail.com>
2011-10-19 22:04:06 -0500

    Initial Commit

---
+++ temp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  temp
+//
+//  Created by Taylor Trimble on 10/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
